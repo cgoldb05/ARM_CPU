851090e2185997478b2f47cb8fc2b05ff96d409b
cgoldb05 <82987537+cgoldb05@users.noreply.github.com>
2021-05-11 01:01:50 -0400

    updated alu
    updated ALUControl to 4 bits

--- ALUDecoder.vhd
+++ ALUDecoder.vhd
@@ -7,7 +7,7 @@ entity ALUDecoder is
 port(
 	func : in std_logic_vector(5 downto 0);
     ALUOp : in std_logic;
-    ALUControl : out std_logic_vector(1 downto 0);
+    ALUControl : out std_logic_vector(3 downto 0);
     FlagW : out std_logic_vector(1 downto 0)
 );
 end ALUDecoder;
@@ -19,15 +19,9 @@ signal cmd : std_logic_vector(3 downto 0);
 begin
 	cmd <= func (4 downto 1);
 	S <= func(0);
-	ALUControl <= "00" when (ALUOp = '0' or (ALUOp = '1' and cmd = "0100")) else -- ADD
-    		       "01" when (ALUOp = '1' and cmd = "0010") else --SUB
-                      "10" when (ALUOp = '1' and cmd = "0000") else --AND
-                      "11" when (ALUOp = '1' and cmd = "1100"); --OR
+	
+	ALUControl <= cmd when (ALUOp = '1') else "0000";
 	FlagW <= "00" when (ALUOp = '0' or S = '0') else                      
-    		 "11" when (S = '1' and (cmd = "0100" or cmd = "0010")) else  -- ADD and SUB
-           	 "10" when (S = '1' and (cmd = "0000" or cmd = "1100")); -- AND and OR
---see table 7.3 
-	--note: all flags are updated with add and sub, but only N and Z for AND and ORR
---FlagW(1) for updating N and Z (flags 3:2)
---FlagW(0) for updating C and V (flags 1:0)
+    		 "11" when (S = '1' and (cmd = "0011" or (cmd = "0100" or cmd = "0010"))) else  -- ADD, SUB, and RSB
+           	 "10" when (S = '1' and (cmd = "1101" or (cmd = "0000" or cmd = "1100"))); -- AND, OR, and MOV
 end synth;
